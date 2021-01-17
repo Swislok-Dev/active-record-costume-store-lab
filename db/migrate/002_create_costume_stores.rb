f378fd0ec5792622ab4f635e98487a7ba6334224
Cory M <cmarker4@gmail.com>
2021-01-17 17:36:06 -0600

    commit tree 13b598df9e14f610ec3a06df8e39ee10f6c3d5b3

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -4,10 +4,10 @@
 class CreateCostumeStores < ActiveRecord::Migration[6.0]
   def change
     create_table :costume_stores do |t|
-      t.text :name
+      t.string :name
       t.string :location
       t.integer :costume_inventory
-      t.integer :number_of_employees
+      t.integer :num_of_employees
       t.boolean :still_in_business
       t.datetime :opening_time
       t.datetime :closing_time
